e21e78f35ddd5e2ca2761de81222418df4e9d917
Thad House <thadhouse1@gmail.com>
2018-12-28 11:59:00 -0800

    Update toolchain tag

--- src/main/java/thad/gradle/ev3/toolchain/Ev3ToolchainExtension.java
+++ src/main/java/thad/gradle/ev3/toolchain/Ev3ToolchainExtension.java
@@ -4,5 +4,5 @@ public class Ev3ToolchainExtension {
   public String versionLow = "6.3";
   public String versionHigh = "6.3";
   public String toolchainVersion = "ev3dev-6.3.0";
-  public String toolchainTag = "v1.0.0";
+  public String toolchainTag = "v1.0.1";
 }
